f59121af374d4459d82d2d55764426172fb622b3
LeoNine Studios <LeoNineStudios@outlook.com>
2018-03-13 22:50:12 -0400

    Formatted message

--- backend/lib/slack.js
+++ backend/lib/slack.js
@@ -3,15 +3,16 @@ let mysql_lib = require('./mysql');
 
 function slack(user, text){
     console.log(user, text)
-    return {
-      "text": "Got it"
-    }
-    // return new Promise(async function (resolve, reject) {
-    //     var response = await mysql_lib.mysql_query("Team espn_ids", `SELECT espn_id FROM mm.team`)
-    //     for (var i = 0, length = response.length; i < length; i++) {
-    //         add_players(response[i]['espn_id'])
-    //     }
-    // })
+    var slack_message = {
+      "username": "Fantasy March Madness",
+      "text": "*Scoreboard*\n"
+    };
+    return new Promise(async function (resolve, reject) {
+        var response = await mysql_lib.mysql_query("Scoreboard", "SELECT * FROM mm.scoreboard")
+        for (var i = 0, length = response.length; i < length; i++) {
+            slack_message.concat(`${response[i]["display_name"]} (${response[i]["name"]}): ${response[i]["total"]}\n`)
+        }
+    })
 }
 
 
