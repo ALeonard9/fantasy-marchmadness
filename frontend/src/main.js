73f460c746f4209e10c4c74c2cc19ad299653b8e
ALeonard9 <ALeonard9@hotmail.com>
2019-03-04 00:27:49 -0500

    Rebirth

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,52 +1,11 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
+import './plugins/vuetify'
+import App from './App.vue'
 import router from './router'
-import {
-  Vuetify,
-  VApp,
-  VNavigationDrawer,
-  VFooter,
-  VList,
-  VBtn,
-  VIcon,
-  VGrid,
-  VToolbar,
-  transitions,
-  VDataTable,
-  VCard,
-  VTextField,
-  VAlert,
-  VSelect
-} from 'vuetify'
-import '../node_modules/vuetify/src/stylus/app.styl'
-
-Vue.use(Vuetify, {
-  components: {
-    VApp,
-    VNavigationDrawer,
-    VFooter,
-    VList,
-    VBtn,
-    VIcon,
-    VGrid,
-    VToolbar,
-    transitions,
-    VDataTable,
-    VCard,
-    VTextField,
-    VAlert,
-    VSelect
-  }
-})
 
 Vue.config.productionTip = false
 
-/* eslint-disable no-new */
 new Vue({
-  el: '#app',
+  render: h => h(App),
   router,
-  components: { App },
-  template: '<App/>'
-})
+}).$mount('#app')
