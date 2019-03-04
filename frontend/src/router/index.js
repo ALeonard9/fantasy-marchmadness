0da18709613a685ce97ff87121d1098cfef505d2
ALeonard9 <ALeonard9@hotmail.com>
2019-03-04 00:27:49 -0500

    Rebirth

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 import Leaderboard from '@/components/Leaderboard'
 import Playerboard from '@/components/Playerboard'
 import Draft from '@/components/Draft'
