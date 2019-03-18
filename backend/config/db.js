b54187a1049cc31edbc286773d1614e51a82fcbc
ALeonard9 <ALeonard9@hotmail.com>
2019-03-17 23:28:33 -0400

    working model

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,5 @@
 module.exports = {
-    host     : process.env.mm_mysql_host || '127.0.0.1',
+    host     : process.env.mm_mysql_host || 'localhost',
     user     : process.env.mm_mysql_user || 'root',
     password : process.env.mm_mysql_pass || 'abc123',
     database : process.env.mm_mysql_db || 'mm',
