c38ddcbc25cfc6f7687517d3d00c74f5be5cc08b
ALeonard9 <ALeonard9@hotmail.com>
2019-03-18 21:57:58 -0400

    fixing views

--- database/mm-sql-init.sql
+++ database/mm-sql-init.sql
@@ -74,11 +74,11 @@ CREATE TABLE `mm`.`game` (
 
 USE `mm`;
 CREATE  OR REPLACE VIEW `scoreboard` AS
-SELECT o.name, o.display_name, o.id, count(case when t.eliminated = 0 then 1 else null end) as players_remaining, sum(round1) as round1, sum(round2) as round2, sum(round3) as round3, sum(round4) as round4, sum(round5) as round5, sum(round6) as round6, (sum(round1) + sum(round2) + sum(round3) + sum(round4) + sum(round5) + sum(round6)) as 'Total'  FROM mm.owner o, mm.player p, mm.team t WHERE p.owner_id = o.id AND p.team_id = t.id group by o.id order by Total desc;
+SELECT o.name, o.display_name, o.id, count(case when t.eliminated = 0 then 1 else null end) as players_remaining, sum(round1) as round1, sum(round2) as round2, sum(round3) as round3, sum(round4) as round4, sum(round5) as round5, sum(round6) as round6, (sum(round1) + sum(round2) + sum(round3) + sum(round4) + sum(round5) + sum(round6)) as 'total'  FROM mm.owner o, mm.player p, mm.team t WHERE p.owner_id = o.id AND p.team_id = t.id group by o.id order by total desc;
 
 USE `mm`;
 CREATE  OR REPLACE VIEW `top_player` AS
-SELECT o.id as owner_id, o.name, o.display_name, full_name, p.id, t.eliminated, (round1 + round2 + round3 + round4 + round5 + round6) as Total FROM mm.player p, mm.owner o, mm.team t WHERE p.owner_id = o.id AND p.team_id = t.id order by Total desc;
+SELECT o.id as owner_id, o.name, o.display_name, full_name, p.id, t.eliminated, (round1 + round2 + round3 + round4 + round5 + round6) as total FROM mm.player p, mm.owner o, mm.team t WHERE p.owner_id = o.id AND p.team_id = t.id order by total desc;
 
 USE `mm`;
 CREATE  OR REPLACE VIEW `draft_randomizer` AS
