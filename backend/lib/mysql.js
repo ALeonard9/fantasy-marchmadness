9f8a0c88225a80ee786ae9f85e4864123884a5f0
ALeonard9 <ALeonard9@hotmail.com>
2019-03-24 02:01:30 -0400

    ESLinting

--- backend/lib/mysql.js
+++ backend/lib/mysql.js
@@ -1,83 +1,59 @@
+'use strict';
+
 const winston = require('winston');
 const chalk = require('chalk');
 const mysql_config = require('../config/db');
 const mysql = require('mysql');
-const request = require('request');
-const async = require('async');
 
-// winston.log('info', chalk.yellow(JSON.stringify(mysql_config)));
+function mysql_query(identifier, query_string) {
+  return new Promise(function(resolve, reject){
+    winston.log('info', chalk.yellow(`${identifier} request received`));
+    var connection = mysql.createConnection(mysql_config);
+    connection.connect(function(err) {
+      if (!err) {
+        connection.query(query_string,
+          function(err, results) {
+            if (!err) {
+              winston.log('info', chalk.yellow('Results: ' + JSON.stringify(results)));
+              connection.end();
+              resolve(results);
+            } else {
+              reject(err);
+              error_handler('Query error: ' + err);
+            }
+          });
+      } else {
+        error_handler('Database connection error: ' + err.message);
+      }
+    });
+  });
+}
+function mysql_query_param(identifier, query_string, param) {
+  return new Promise(function(resolve, reject){
+    winston.log('info', chalk.yellow(`${identifier} ${JSON.stringify(param)} request received`));
+    var connection = mysql.createConnection(mysql_config);
+    connection.connect(function(err) {
+      if (!err) {
+        connection.query(query_string, param,
+          function(err, results) {
+            if (!err) {
+              winston.log('info', chalk.yellow('Results: ' + JSON.stringify(results)));
+              connection.end();
+              resolve(results);
+            } else {
+              reject(err);
+              error_handler('Query error: ' + err);
+            }
+          });
+      } else {
+        error_handler('Database connection error: ' + err.message);
+      }
+    });
+  });
+}
 
-function mysql_query (identifier, query_string) {
-    return new Promise(function(resolve, reject){
-        winston.log('info', chalk.yellow(`${identifier} request received`));
-        var connection = mysql.createConnection(mysql_config);
-        connection.connect(function(err) {
-          if (!err) {
-              connection.query(query_string,
-                  function(err, results) {
-                      if (!err) {
-                          winston.log('info', chalk.yellow('Results: ' + JSON.stringify(results)));
-                          connection.end();
-                          resolve(results);
-                      } else {
-                          reject(err);
-                          error_handler('Query error: ' + err);
-                      }
-                  });
-          } else {
-              error_handler('Database connection error: ' + err.message);
-          }
-        });
-    })
+function error_handler(message) {
+  winston.log('error', chalk.red(message));
 }
-function mysql_query_param (identifier, query_string, param) {
-    return new Promise(function(resolve, reject){
-        winston.log('info', chalk.yellow(`${identifier} ${JSON.stringify(param)} request received`));
-        var connection = mysql.createConnection(mysql_config);
-        connection.connect(function(err) {
-          if (!err) {
-              connection.query(query_string, param,
-                  function(err, results) {
-                      if (!err) {
-                          winston.log('info', chalk.yellow('Results: ' + JSON.stringify(results)));
-                          connection.end();
-                          resolve(results);
-                      } else {
-                          reject(err);
-                          error_handler('Query error: ' + err);
-                      }
-                  });
-          } else {
-              error_handler('Database connection error: ' + err.message);
-          }
-        });
-    })
-}
-function mysql_query_post (identifier, query_string, hash) {
-    return new Promise(function(resolve, reject){
-        winston.log('info', chalk.yellow(`${identifier} ${param} post received`));
-        var connection = mysql.createConnection(mysql_config);
-        connection.connect(function(err) {
-          if (!err) {
-              connection.query(query_string, [param],
-                  function(err, results) {
-                      if (!err) {
-                          winston.log('info', chalk.yellow('Results: ' + JSON.stringify(results)));
-                          connection.end();
-                          resolve(results);
-                      } else {
-                          reject(err);
-                          error_handler('Query error: ' + err);
-                      }
-                  });
-          } else {
-              error_handler('Database connection error: ' + err.message);
-          }
-        });
-    })
-}
-function error_handler (message) {
-    winston.log('error', chalk.red(message));  
-  }
-  
-module.exports = {mysql_query, mysql_query_param}
+
+module.exports = {mysql_query, mysql_query_param};
--- backend/lib/slack.js
+++ backend/lib/slack.js
@@ -1,32 +1,25 @@
-let request = require('request');
-let mysql_lib = require('./mysql');
+'use strict';
+
+const mysql_lib = require('./mysql');
 
 function slack(user, text){
-    return new Promise(async function (resolve, reject) {
-        var slack_message = {
-          "username": "Fantasy March Madness",
-          "text": "*Scoreboard*\n"
-        };
-        var response = await mysql_lib.mysql_query("Scoreboard", "SELECT * FROM mm.scoreboard")
-        for (var i = 0, length = response.length; i < length; i++) {
-            var addition = `${response[i]["display_name"]} (${response[i]["name"]}): ${response[i]["total"]}\n`
-            slack_message.text = slack_message.text.concat(addition)
-            if (i === response.length - 1) {
-                console.log(JSON.stringify(slack_message))
-                resolve(slack_message);
-            }
-        }
-    })
-}
-
-
-function rejected(result) {
-    console.log(result);
-}
-function resolved(result) {
-    console.log('Resolved');
+  return new Promise(async function(resolve, reject) {
+    var slack_message = {
+      username: 'Fantasy March Madness',
+      text: '*Scoreboard*\n',
+    };
+    var response = await mysql_lib.mysql_query('Scoreboard', 'SELECT * FROM mm.scoreboard');
+    for (var i = 0, length = response.length; i < length; i++) {
+      var addition = `${response[i]['display_name']} (${response[i]['name']}): ${response[i]['total']}\n`;
+      slack_message.text = slack_message.text.concat(addition);
+      if (i === response.length - 1) {
+        console.log(JSON.stringify(slack_message));
+        resolve(slack_message);
+      }
+    }
+  });
 }
 
 module.exports = {
-    slack
+  slack,
 };
