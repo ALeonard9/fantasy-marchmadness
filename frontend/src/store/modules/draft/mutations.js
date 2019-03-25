6aa1b5cf51fc48f45fb3469094584550d27aec8d
Alex Cromer <alex.cromer6@gmail.com>
2019-03-24 21:59:56 -0400

    adding mutations to project

---
+++ frontend/src/store/modules/draft/mutations.js
@@ -0,0 +1,11 @@
+import DRAFT_PLAYER_REQUEST from './mutation_types'
+
+const mutations = {
+
+    [DRAFT_PLAYER_REQUEST] (state, {draftSelection}) {
+        state.draftSelection = draftSelection
+    }
+
+}
+
+export default mutations
