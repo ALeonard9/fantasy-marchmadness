d545fff1fb56378743b9b1629fc6b6326511e12c
LeoNine Studios <LeoNineStudios@outlook.com>
2020-02-22 19:58:06 -0500

    Resilient local database

--- database/dummy-data.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `mm`.`owner` (`name`, `display_name`) VALUES ('Adam', 'ALEO');
-INSERT INTO `mm`.`owner` (`name`, `display_name`) VALUES ('Josh', 'JFO');
-INSERT INTO `mm`.`owner` (`name`, `display_name`) VALUES ('Ryan', 'RKO');
-INSERT INTO `mm`.`owner` (`name`, `display_name`) VALUES ('Cody', 'CAU');
